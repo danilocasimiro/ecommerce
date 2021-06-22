90dfd7ef9522d6cc0b7bb3f420c939b4271c85b1
Danilo Casimiro <dan_casimiro@hotmail.com>
2021-06-22 14:24:23 -0300

    Menu pedidos admin

--- vendor/hcodebr/php-classes/src/Model/OrderStatus.php
+++ vendor/hcodebr/php-classes/src/Model/OrderStatus.php
@@ -11,5 +11,12 @@ class OrderStatus extends Model {
   const AGUARDANDO_PAGAMENTO = 3;
   const PAGO = 4;
   const ENTREGUE = 5;
- 
+
+  public static function listAll()
+  {
+    $sql = new Sql();
+
+    return $sql->select("SELECT * FROM tb_ordersstatus ORDER BY desstatus DESC");
+
+  }
 }
