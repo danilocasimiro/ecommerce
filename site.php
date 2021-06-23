1b97a558dd143005626a35b64589651480c5aa9c
Danilo Casimiro <dan_casimiro@hotmail.com>
2021-06-23 09:34:19 -0300

    integracao com pagseguro

--- site.php
+++ site.php
@@ -252,7 +252,7 @@ $app->post("/checkout", function() {
 
 	$order->save();
 
-	header("Location: /order/".$order->getidorder());
+	header("Location: /order/".$order->getidorder()."/pagseguro");
 	exit;
  });
 
@@ -640,5 +640,33 @@ $app->post("/profile/change-password", function() {
 
 	header("Location: /profile/change-password");
 	exit;
+
+});
+
+$app->get("/order/:idorder/pagseguro", function($idorder) {
+
+	User::verifyLogin(false);
+
+	$order = new Order();
+
+	$order->get((int)$idorder);
+
+	$cart = $order->getCart();
+
+	$page = new Page([
+		'header'=>false,
+		'footer'=>false
+	]);
+
+	$page->setTpl("payment-pagseguro", [
+		'order'=>$order->getValues(),
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts(),
+		'phone'=>[
+			'areaCode'=>substr($order->getnrphone(), 0, 2),
+			'number'=>substr($order->getnrphone(), 2, strlen($order->getnrphone()))
+		]
+	]);
+
 
 });
